f96235fb496c30590b33fc89ebb23d7768cb740b
franzmikael <franzmikael11@gmail.com>
2022-08-30 17:14:29 +0700

    Add Header navbar component & update styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from 'pages/LandingPage';
 import 'assets/scss/style.scss';
 
 function App() {
   return (
     <div className="App">
-      hi dunia!
+      <Router>
+        <Routes>
+          <Route path='/' element={<LandingPage/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import propTypes from 'prop-types'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import propTypes from 'prop-types';
 
 export default function Button(props) {
   const className = [props.className];
